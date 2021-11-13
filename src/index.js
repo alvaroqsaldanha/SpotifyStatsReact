8f4d2c6e18fc574f2ab318cf8828a954f4012617
Álvaro Saldanha <alvaroqsaldanha@gmail.com>
2021-11-13 17:19:21 +0000

    Minor changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+/*global chrome*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
+import App from './containers/App'; 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -10,9 +11,7 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
